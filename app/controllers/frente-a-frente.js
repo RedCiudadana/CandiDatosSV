bf790e79868e5746a0691689c4449c901399c209
D3AH <diegoeauyonh@gmail.com>
2019-01-02 15:24:58 -0600

    Agregar colaboradores al footer.

--- app/controllers/frente-a-frente.js
+++ app/controllers/frente-a-frente.js
@@ -4,12 +4,10 @@ import { computed } from '@ember/object';
 export default Controller.extend({
 
   availableInfoUno: computed('perfilUno', function() {
-    console.log('Calculando 1');
     return this.get('model').info.findBy('id', this.get('perfilUno.id'));
   }),
 
   availableInfoDos: computed('perfilDos', function() {
-    console.log('Calculando 2');
     return this.get('model').info.findBy('id', this.get('perfilDos.id'));
   })
 });
