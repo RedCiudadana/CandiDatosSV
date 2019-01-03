492dd242414074579b60f5e939a0456a45617b3b
D3AH <diegoeauyonh@gmail.com>
2019-01-03 15:57:28 -0600

    Agregando miembros a partido.

--- app/routes/partidos/partido.js
+++ app/routes/partidos/partido.js
@@ -1,8 +1,15 @@
 import Route from '@ember/routing/route';
+import { A } from '@ember/array';
 
 export default Route.extend({
     model(params) {
         const party = this.store.peekRecord('partido', params.id);
-        return party;
+        let miembros = A(this.modelFor('application')).presidents.filter((data) => {
+            return data.partido.content === party;
+        });
+        return {
+          party: party,
+          miembros: miembros
+        };
     }
 });
